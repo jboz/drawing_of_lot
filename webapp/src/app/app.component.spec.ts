f5cd06df0e951b906318f77897d7b66c0a117e59
Julien Boz <julienboz@gmail.com>
2020-02-18 20:56:34 +0100

    jest configuration

--- webapp/src/app/app.component.spec.ts
+++ webapp/src/app/app.component.spec.ts
@@ -1,12 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
@@ -14,18 +12,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'drawing-a-lot'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('drawing-a-lot');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('drawing-a-lot app is running!');
   });
 });
--- webapp/src/app/group-list/group-list.component.spec.ts
+++ webapp/src/app/group-list/group-list.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GroupListComponent } from './group-list.component';
 
 describe('GroupListComponent', () => {
@@ -8,9 +7,8 @@ describe('GroupListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GroupListComponent ]
-    })
-    .compileComponents();
+      declarations: [GroupListComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
