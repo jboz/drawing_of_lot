00c6ff8ab081d8d30e4bf4ac4e86a33ad94ba54c
Julien Boz <julienboz@gmail.com>
2020-04-08 19:47:53 +0200

    amélioration réponses

--- functions/src/handlers/intent/next.intent.handler.ts
+++ functions/src/handlers/intent/next.intent.handler.ts
@@ -1,14 +1,21 @@
 import { Contexts, DialogflowConversation } from 'actions-on-google';
 import admin from 'firebase-admin';
 import random from 'lodash/random';
-import replace from 'lodash/replace';
 import { ConversationData } from '../../domain/conversation-data-model';
 import { GroupUndefinedError } from '../../domain/group-undefined.error';
 import { byGroupName } from '../../domain/random.service';
 import IntentHandler from './intent.handler';
 
-const RESULTATS = [`Le résultat du tirage au sort est : {}`, `Le résultat est : {}`, '', 'Voilà : {}!'];
+const RESULTATS = [`Le résultat du tirage au sort est: `, `Le résultat est: `, '', 'Voilà: ', 'ce sera: '];
 const RESPONSES = [`Autre chose ?`, `Que puis-je faire d'autres ?`, `Est-ce que cela vous convient ?`];
+
+const getResult = (member: string) => {
+  return RESULTATS[random(0, RESULTATS.length - 1)] + member;
+};
+
+const getQuestion = () => {
+  return RESPONSES[random(0, RESPONSES.length - 1)];
+};
 
 export default class NextIntentHandler implements IntentHandler {
   public name = 'next_intent';
@@ -23,16 +30,8 @@ export default class NextIntentHandler implements IntentHandler {
       .auth()
       .getUserByEmail(conv.user.email || '')
       .then((user) => byGroupName(user.uid, group, purpose))
-      .then((member) => conv.ask(this.getResult(member)))
-      .then((_) => conv.ask(this.getQuestion()))
+      .then((member) => conv.ask(getResult(member)))
+      .then((_) => conv.ask(getQuestion()))
       .then((_) => (conv.user.storage = { group, purpose }));
-  }
-
-  private getResult(member: string) {
-    return replace(RESULTATS[random(0, RESULTATS.length - 1)], '{}', member);
-  }
-
-  private getQuestion() {
-    return RESPONSES[random(0, RESPONSES.length - 1)];
   }
 }
